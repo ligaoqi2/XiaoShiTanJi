534508ec6d9faaee954393cd2ffa20f5fc53e0e6
junfengTan <junfeng.tan@izhijue.com>
2023-12-31 00:07:01 +0800

    chore: init

--- web/src/components/juejin/splitVideo/splitVideo.vue
+++ web/src/components/juejin/splitVideo/splitVideo.vue
@@ -2,14 +2,25 @@
   <div class="splitVideo_content">
     <div class="title">
       <h1>分片加载视频的实现，边播放边加载</h1>
+      <el-link
+        type="primary"
+        @click="go('https://juejin.cn/post/7263776537749323831#heading-16')"
+        >博客地址 <Link style="width: 1em; height: 1em; margin-right: 8px"
+      /></el-link>
     </div>
     <div class="splitVideo_body">
       <div class="title_line">
         <h2>Blob</h2>
       </div>
       <div class="link_line">
-        <el-link type="primary" @click="goGitHub"
-          >源码 <Link style="width: 1em; height: 1em; margin-right: 8px"
+        <el-link
+          type="primary"
+          @click="
+            go(
+              'https://github.com/nt1107/XiaoShiTanJi/blob/main/web/src/components/juejin/splitVideo/Blob.vue'
+            )
+          "
+          >前端代码 <Link style="width: 1em; height: 1em; margin-right: 8px"
         /></el-link>
       </div>
       <div class="body_line">
@@ -21,8 +32,14 @@
         <h2>mediaSource</h2>
       </div>
       <div class="link_line">
-        <el-link type="primary" @click="goGitHub"
-          >源码 <Link style="width: 1em; height: 1em; margin-right: 8px"
+        <el-link
+          type="primary"
+          @click="
+            go(
+              'https://github.com/nt1107/XiaoShiTanJi/blob/main/web/src/components/juejin/splitVideo/mediaSource.vue'
+            )
+          "
+          >前端代码 <Link style="width: 1em; height: 1em; margin-right: 8px"
         /></el-link>
       </div>
       <div class="body_line">
@@ -38,14 +55,19 @@ import Blob from './Blob.vue'
 import mediaSource from './mediaSource.vue'
 import { Link } from '@element-plus/icons-vue'
 
-const init = () => {}
+const go = (url) => {
+  window.open(url)
+}
 
 onMounted(() => {})
 </script>
 
 <style lang="less" scoped>
 .splitVideo_content {
+  background-color: white;
+  padding: 20px;
   .title {
+    text-align: center;
     h1 {
       text-align: center;
     }
